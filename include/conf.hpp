372364c2d454408d75a16255e6c6ade6fa0263b7
Feglawy <a.m9518520963@outlook.com>
2024-09-29 04:18:47 +0300

    init commit

---
+++ include/conf.hpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define fast ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+#define read (void)freopen("../input.txt", "r", stdin);
+#define write (void)freopen("../output.txt", "w", stdout);
+#define clear system("cls");
+#define pause system("pause");
+#define makeConsoleUTF8 system("chcp 65001");
+
+#define all(v) (v).begin(), (v).end()
+
+typedef long long ll;
+typedef double long dl;
+typedef unsigned long long ull;
+
+
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "conf.hpp"
+
+int main()
+{
+    // read; write;
+    // makeConsoleUTF8;
+
+    return EXIT_SUCCESS;
+}
